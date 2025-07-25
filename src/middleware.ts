36a65f7118104ecc36ce23ad555c303a2a70dc50
Tu Nombre <lgarbate@gmail.com>
2025-07-25 19:45:50 -0300

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export default withAuth(
             authorized: async ({ req: { cookies } }) => {
                 const secureCookie = process.env.NEXTAUTH_URL?.startsWith("https://") ?? !!process.env.VERCEL;
                 const cookieName = secureCookie ? "__Secure-next-auth.session-token" : "next-auth.session-token";
-                const session = await (await fetch('http://localhost:3000/api/auth/session', { method: 'GET', headers: { 'Cookie': `${cookieName}=${cookies.get(cookieName)?.value}` } })).json();
+                const session = await (await fetch(`${process.env.NEXTAUTH_URL}/api/auth/session`, { method: 'GET', headers: { 'Cookie': `${cookieName}=${cookies.get(cookieName)?.value}` } })).json();
                 return !!session.user;
             },
         },
